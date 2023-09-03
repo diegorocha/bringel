25c951947dce76ac667709adce2cb6ab8ae3c288
Diego Rocha <diego@diegorocha.com.br>
2023-09-03 20:17:39 -0300

    feat: add swagger

--- src/bringel/settings.py
+++ src/bringel/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- src/store/urls.py
+++ src/store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
 from store.api.routes import router
+from store.api.swagger import swagger_urls
 
 app_name = 'store'
 urlpatterns = [
     path('api/', include(router.urls)),
 ]
+urlpatterns.extend(swagger_urls)
