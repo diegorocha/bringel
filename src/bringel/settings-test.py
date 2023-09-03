a253ccc760edcef3e74e7b2792300888c0eb94c9
Diego Rocha <diego@diegorocha.com.br>
2023-09-03 14:18:53 -0300

    feat: setting up tests runner and coverage report

---
+++ src/bringel/settings-test.py
@@ -0,0 +1,11 @@
+from bringel.settings import *
+
+
+# Override database for testing
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'ATOMIC_REQUESTS': True,
+    }
+}
