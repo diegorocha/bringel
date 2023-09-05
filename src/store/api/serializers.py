3b6186ab045f27e6815db786e93272413dc57856
Diego Rocha <diego@diegorocha.com.br>
2023-09-04 22:01:59 -0300

    feat: specifying fields on serializers for better api docs

--- src/store/api/serializers.py
+++ src/store/api/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework.fields import ReadOnlyField, CharField, FloatField
-from rest_framework.relations import SlugRelatedField, StringRelatedField, PrimaryKeyRelatedField
+from rest_framework.fields import CharField, FloatField, IntegerField, BooleanField, DecimalField
+from rest_framework.relations import SlugRelatedField, PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer
 
 from store.models import Tag, Supplier, ProductVariant, Product, CustomerRating
@@ -9,12 +9,17 @@ class TagSerializer(ModelSerializer):
     class Meta:
         model = Tag
         fields = ['id', 'name', 'description']
+    id = IntegerField(label='Id', help_text='Tag ID', read_only=True)
+    name = CharField(label='Name', help_text='Tag name', max_length=50, required=True)
+    description = CharField(label='Description', help_text='Tag description', max_length=100, required=False)
 
 
 class SupplierSerializer(ModelSerializer):
     class Meta:
         model = Supplier
         fields = ['id', 'name']
+    id = IntegerField(label='Id', help_text='Supplier ID', read_only=True)
+    name = CharField(label='Name', help_text='Supplier name', max_length=100, required=True)
 
 
 class ProductVariantSerializer(ModelSerializer):
@@ -22,20 +27,36 @@ class ProductVariantSerializer(ModelSerializer):
         model = ProductVariant
         fields = ['id', 'product', 'variant_name', 'variant_value', 'sku', 'in_stock', 'price']
 
+    id = IntegerField(label='Id', help_text='ProductVariant ID', read_only=True)
+    product = PrimaryKeyRelatedField(label='Product', help_text='Product ID', queryset=Product.objects.all(),
+                                     required=True)
+    variant_name = CharField(label='Variant Name', help_text='Name of variant', max_length=20, required=True)
+    variant_value = CharField(label='Variant Value', help_text='Value of variant', max_length=50, required=True)
+    sku = CharField(label='SKU', help_text='SKU code', max_length=8, required=True)
+    in_stock = BooleanField(label='In Stock', help_text='', default=False)
+    price = DecimalField(label='Price', max_digits=8, decimal_places=2, required=True)
+
 
 class ProductVariantNestedSerializer(ModelSerializer):
     class Meta:
         model = ProductVariant
         fields = ['id', 'variant_name', 'variant_value', 'sku', 'in_stock', 'price']
+        ref_name = 'ProductVariantRead'
+    id = IntegerField(label='Id', help_text='ProductVariant ID', read_only=True)
+    variant_name = CharField(label='Variant Name', help_text='Name of variant', max_length=20, required=True)
+    variant_value = CharField(label='Variant Value', help_text='Value of variant', max_length=50, required=True)
+    sku = CharField(label='SKU', help_text='SKU code', max_length=8, required=True)
+    in_stock = BooleanField(label='In Stock', help_text='', default=False)
+    price = DecimalField(label='Price', max_digits=8, decimal_places=2, required=True)
 
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'supplier', 'name', 'description', 'tags', 'rating', 'variants']
-    supplier = SlugRelatedField(slug_field='name', queryset=Supplier.objects.all())
-    tags = SlugRelatedField(slug_field='name', many=True, queryset=Tag.objects.all())
-    rating = FloatField(read_only=True)
+    supplier = SlugRelatedField(slug_field='name', help_text='Supplier Name',  queryset=Supplier.objects.all())
+    tags = SlugRelatedField(slug_field='name', help_text='Tag Name',  many=True, queryset=Tag.objects.all())
+    rating = FloatField(help_text='Calculated rating', read_only=True)
     variants = ProductVariantNestedSerializer(many=True, read_only=True)
 
 
@@ -43,21 +64,25 @@ class CustomerRatingSerializer(ModelSerializer):
     class Meta:
         model = CustomerRating
         fields = ['product', 'rating', 'description', 'created_at']
-    rating = FloatField(min_value=1, max_value=5)
+    product = PrimaryKeyRelatedField(label='Product', help_text='Product ID', queryset=Product.objects.all(),
+                                     required=True)
+    rating = IntegerField(help_text='Rating from 1 to 5',  min_value=1, max_value=5)
 
 
 class CustomerRatingReadSerializer(ModelSerializer):
     class Meta:
         model = CustomerRating
         fields = ['rating', 'description', 'created_at']
+    rating = IntegerField(help_text='Rating from 1 to 5', min_value=1, max_value=5)
 
 
 class ProductDetailSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'supplier', 'name', 'description', 'tags', 'rating', 'ratings', 'variants']
-    supplier = SlugRelatedField(slug_field='name', queryset=Supplier.objects.all())
-    tags = SlugRelatedField(slug_field='name', many=True, queryset=Tag.objects.all())
-    rating = FloatField(read_only=True)
+        ref_name = 'DetailedProduct'
+    supplier = SlugRelatedField(slug_field='name', help_text='Supplier Name', queryset=Supplier.objects.all())
+    tags = SlugRelatedField(slug_field='name', help_text='Tag Name', many=True, queryset=Tag.objects.all())
+    rating = FloatField(help_text='Calculated rating', min_value=1, max_value=5, read_only=True)
     variants = ProductVariantNestedSerializer(many=True, read_only=True)
     ratings = CustomerRatingReadSerializer(many=True)
--- src/store/api/swagger.py
+++ src/store/api/swagger.py
@@ -5,12 +5,9 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Store Products",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      description="API to manage store products, variants, ratings",
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
