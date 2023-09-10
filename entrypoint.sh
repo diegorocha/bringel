1c3ba0f7d5a852fe130c8b46e90ab49032f82f1f
Diego Rocha <diego@diegorocha.com.br>
2023-09-10 15:59:28 -0300

    chore: single entrypoint for web and worker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-echo "Preparing static files"
-python manage.py collectstatic --no-input --no-color
+MODE="${APP_MODE:=web}"
 
-echo "Starting webserver"
-gunicorn -b :80 bringel.wsgi
+if [ "$MODE" = "web" ]; then
+  echo "Running web mode"
+  echo "Preparing static files"
+  python manage.py collectstatic --no-input --no-color
+  echo "Starting webserver"
+  gunicorn -b :80 bringel.wsgi
+else
+  echo "Running worker mode"
+  echo "Starting worker"
+  celery -A bringel worker -l INFO
+fi
